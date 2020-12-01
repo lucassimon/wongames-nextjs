d34049be24e33d3068b103d87f5c90262069eb05
Lucas <lucassrod@gmail.com>
2020-12-01 11:11:05 -0300

    Fix import ButtonProps

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,7 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import { darken } from 'polished'
 
-import { ButtonProps } from '.'
+import { ButtonProps } from './types'
 
 export type WrapperProps = {
   hasIcon: boolean
