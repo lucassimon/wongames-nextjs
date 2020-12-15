0ff163de78981fa295f4a34eb436f8468a73f602
Lucas <lucassrod@gmail.com>
2020-12-15 17:51:00 -0300

    Create TextField

---
+++ src/components/TextField/test.tsx
@@ -0,0 +1,117 @@
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Email } from '@styled-icons/material-outlined'
+
+import { renderWithLightTheme } from 'utils/tests/helpers'
+
+import TextField from '.'
+
+describe('<TextField />', () => {
+  it('Renders with Label', () => {
+    renderWithLightTheme(<TextField label="Label" name="Label" />)
+
+    expect(screen.getByLabelText('Label')).toBeInTheDocument()
+  })
+
+  it('Renders without Label', () => {
+    renderWithLightTheme(<TextField />)
+
+    expect(screen.queryByLabelText('Label')).not.toBeInTheDocument()
+  })
+
+  it('Renders with placeholder', () => {
+    renderWithLightTheme(<TextField placeholder="hey you" />)
+
+    expect(screen.getByPlaceholderText('hey you')).toBeInTheDocument()
+  })
+
+  it('Renders with Icon', () => {
+    renderWithLightTheme(<TextField icon={<Email data-testid="icon" />} />)
+
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+
+  it('Renders with Icon on the right side', () => {
+    renderWithLightTheme(
+      <TextField icon={<Email data-testid="icon" />} iconPosition="right" />
+    )
+
+    expect(screen.getByTestId('icon').parentElement).toHaveStyle({ order: 1 })
+  })
+
+  it('Changes its value when typing', async () => {
+    const onInput = jest.fn()
+    renderWithLightTheme(
+      <TextField onInput={onInput} label="TextField" name="TextField" />
+    )
+
+    const input = screen.getByRole('textbox')
+    const text = 'This is my new text'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text)
+      expect(onInput).toHaveBeenCalledTimes(text.length)
+    })
+    expect(onInput).toHaveBeenCalledWith(text)
+  })
+
+  it('Does not changes its value when disabled', async () => {
+    const onInput = jest.fn()
+    renderWithLightTheme(
+      <TextField
+        onInput={onInput}
+        label="TextField"
+        name="TextField"
+        disabled
+      />
+    )
+
+    const input = screen.getByRole('textbox')
+    expect(input).toBeDisabled()
+
+    const text = 'This is my new text'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).not.toHaveValue(text)
+    })
+    expect(onInput).not.toHaveBeenCalled()
+  })
+
+  it('Renders with error', () => {
+    const { container } = renderWithLightTheme(
+      <TextField
+        icon={<Email data-testid="icon" />}
+        label="TextField"
+        error="Error message"
+      />
+    )
+
+    expect(screen.getByText('Error message')).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('Is accessible by tab', () => {
+    renderWithLightTheme(<TextField label="TextField" name="TextField" />)
+
+    const input = screen.getByLabelText('TextField')
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+    expect(input).toHaveFocus()
+  })
+
+  it('Is not accessible by tab when disabled', () => {
+    renderWithLightTheme(
+      <TextField label="TextField" name="TextField" disabled />
+    )
+
+    const input = screen.getByLabelText('TextField')
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+    expect(input).not.toHaveFocus()
+  })
+})
